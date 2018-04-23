8be51ff282076bf6a41e891c2496be7d5c1c2630
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/App.xaml.cs
@@ -0,0 +1,67 @@
+﻿using DevExpress.Core;
+using DevExpress.Themes.ColorKeys;
+using System;
+using Windows.ApplicationModel;
+using Windows.ApplicationModel.Activation;
+using Windows.UI;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+
+namespace ColosSchemesDemo1 {
+    sealed partial class App : Application {
+        public App() {
+            OverrideThemeColors();
+
+            this.InitializeComponent();
+            this.Suspending += OnSuspending;
+        }
+
+        void OverrideThemeColors() {
+            ThemeManager.GenericColorScheme.AllScopes.RibbonControlColors[RibbonControlColorKeys.HeaderBackground] = new DevExpress.Themes.ColorSet(
+                Color.FromArgb(0xFF, 0x41, 0x83, 0x66),
+                Color.FromArgb(0xFF, 0x16, 0x4B, 0x2F));
+            ThemeManager.GenericColorScheme.AllScopes.RibbonHeaderItemColors[RibbonHeaderItemColorKeys.ForegroundSelected] =
+            ThemeManager.GenericColorScheme.AllScopes.RibbonHeaderContextualItemColors[RibbonHeaderContextualItemColorKeys.ForegroundSelected] = new DevExpress.Themes.ColorSet(
+                Color.FromArgb(0xFF, 0x41, 0x83, 0x66),
+                Color.FromArgb(0xFF, 0x16, 0x4B, 0x2F));
+            ThemeManager.GenericColorScheme.AllScopes.RibbonHeaderContextualItemColors[RibbonHeaderContextualItemColorKeys.Background] = Color.FromArgb(0x33, 0x00, 0x00, 0x00);
+            ThemeManager.GenericColorScheme.AllScopes.BackstageViewColors[BackstageViewColorKeys.Background] = Color.FromArgb(0xAA, 0xFF, 0xFF, 0xFF);
+            ThemeManager.GenericColorScheme.AllScopes.BackstageViewColors[BackstageViewColorKeys.TabContentBackground] = Colors.Transparent;
+            
+            ThemeManager.Win8ColorScheme.AllScopes.RibbonControlColors[RibbonControlColorKeys.HeaderBackground] = new DevExpress.Themes.ColorSet(
+                Color.FromArgb(0xFF, 0x1B, 0x47, 0x8A),
+                Color.FromArgb(0xFF, 0x09, 0x34, 0x67));
+            ThemeManager.Win8ColorScheme.AllScopes.RibbonHeaderContextualItemColors[RibbonHeaderContextualItemColorKeys.ForegroundSelected] =
+            ThemeManager.Win8ColorScheme.AllScopes.RibbonHeaderItemColors[RibbonHeaderItemColorKeys.ForegroundSelected] = new DevExpress.Themes.ColorSet(
+                Color.FromArgb(0xFF, 0x2B, 0x57, 0x9A),
+                Color.FromArgb(0xFF, 0x1B, 0x47, 0x8A));
+            ThemeManager.Win8ColorScheme.AllScopes.RibbonHeaderContextualItemColors[RibbonHeaderContextualItemColorKeys.Background] = Color.FromArgb(0x33, 0x00, 0x00, 0x00);
+            ThemeManager.Win8ColorScheme.AllScopes.BackstageViewColors[BackstageViewColorKeys.Background] = Color.FromArgb(0x77, 0xFF, 0xFF, 0xFF);
+            ThemeManager.Win8ColorScheme.AllScopes.BackstageViewColors[BackstageViewColorKeys.TabContentBackground] = Colors.Transparent;
+        }
+
+        protected override void OnLaunched(LaunchActivatedEventArgs e) {
+            Frame rootFrame = Window.Current.Content as Frame;
+            if(rootFrame == null) {
+                rootFrame = new Frame();
+                rootFrame.NavigationFailed += OnNavigationFailed;
+                Window.Current.Content = rootFrame;
+            }
+
+            if(e.PrelaunchActivated == false) {
+                if(rootFrame.Content == null)
+                    rootFrame.Navigate(typeof(MainPage), e.Arguments);
+
+                Window.Current.Activate();
+            }
+        }
+
+        void OnNavigationFailed(object sender, NavigationFailedEventArgs e) {
+            throw new Exception("Failed to load Page " + e.SourcePageType.FullName);
+        }
+        void OnSuspending(object sender, SuspendingEventArgs e) {
+            e.SuspendingOperation.GetDeferral().Complete();
+        }
+    }
+}
---
+++ VB/ColosSchemesDemo1/App.xaml.vb
@@ -0,0 +1,68 @@
+﻿Imports DevExpress.Core
+Imports DevExpress.Themes.ColorKeys
+Imports Windows.UI
+
+NotInheritable Class App
+    Inherits Application
+
+    Public Sub New()
+        OverrideThemeColors()
+
+        InitializeComponent()
+    End Sub
+
+    Private Sub OverrideThemeColors()
+        ThemeManager.GenericColorScheme.AllScopes.RibbonControlColors(RibbonControlColorKeys.HeaderBackground) = New DevExpress.Themes.ColorSet(Color.FromArgb(&HFF, &H41, &H83, &H66), Color.FromArgb(&HFF, &H16, &H4B, &H2F))
+        ThemeManager.GenericColorScheme.AllScopes.RibbonHeaderItemColors(RibbonHeaderItemColorKeys.ForegroundSelected) = New DevExpress.Themes.ColorSet(Color.FromArgb(&HFF, &H41, &H83, &H66), Color.FromArgb(&HFF, &H16, &H4B, &H2F))
+        ThemeManager.GenericColorScheme.AllScopes.RibbonHeaderContextualItemColors(RibbonHeaderContextualItemColorKeys.ForegroundSelected) = New DevExpress.Themes.ColorSet(Color.FromArgb(&HFF, &H41, &H83, &H66), Color.FromArgb(&HFF, &H16, &H4B, &H2F))
+        ThemeManager.GenericColorScheme.AllScopes.RibbonHeaderContextualItemColors(RibbonHeaderContextualItemColorKeys.Background) = Color.FromArgb(&H33, &H0, &H0, &H0)
+        ThemeManager.GenericColorScheme.AllScopes.BackstageViewColors(BackstageViewColorKeys.Background) = Color.FromArgb(&HAA, &HFF, &HFF, &HFF)
+        ThemeManager.GenericColorScheme.AllScopes.BackstageViewColors(BackstageViewColorKeys.TabContentBackground) = Colors.Transparent
+
+        ThemeManager.Win8ColorScheme.AllScopes.RibbonControlColors(RibbonControlColorKeys.HeaderBackground) = New DevExpress.Themes.ColorSet(Color.FromArgb(&HFF, &H1B, &H47, &H8A), Color.FromArgb(&HFF, &H9, &H34, &H67))
+        ThemeManager.Win8ColorScheme.AllScopes.RibbonHeaderContextualItemColors(RibbonHeaderContextualItemColorKeys.ForegroundSelected) = New DevExpress.Themes.ColorSet(Color.FromArgb(&HFF, &H2B, &H57, &H9A), Color.FromArgb(&HFF, &H1B, &H47, &H8A))
+        ThemeManager.Win8ColorScheme.AllScopes.RibbonHeaderItemColors(RibbonHeaderItemColorKeys.ForegroundSelected) = New DevExpress.Themes.ColorSet(Color.FromArgb(&HFF, &H2B, &H57, &H9A), Color.FromArgb(&HFF, &H1B, &H47, &H8A))
+        ThemeManager.Win8ColorScheme.AllScopes.RibbonHeaderContextualItemColors(RibbonHeaderContextualItemColorKeys.Background) = Color.FromArgb(&H33, &H0, &H0, &H0)
+        ThemeManager.Win8ColorScheme.AllScopes.BackstageViewColors(BackstageViewColorKeys.Background) = Color.FromArgb(&H77, &HFF, &HFF, &HFF)
+        ThemeManager.Win8ColorScheme.AllScopes.BackstageViewColors(BackstageViewColorKeys.TabContentBackground) = Colors.Transparent
+    End Sub
+
+    Protected Overrides Sub OnLaunched(e As Windows.ApplicationModel.Activation.LaunchActivatedEventArgs)
+#If DEBUG Then
+        If System.Diagnostics.Debugger.IsAttached Then
+            Me.DebugSettings.EnableFrameRateCounter = True
+        End If
+#End If
+
+        Dim rootFrame As Frame = TryCast(Window.Current.Content, Frame)
+
+        If rootFrame Is Nothing Then
+            rootFrame = New Frame()
+
+            AddHandler rootFrame.NavigationFailed, AddressOf OnNavigationFailed
+
+            If e.PreviousExecutionState = ApplicationExecutionState.Terminated Then
+            End If
+
+            Window.Current.Content = rootFrame
+        End If
+
+        If e.PrelaunchActivated = False Then
+            If rootFrame.Content Is Nothing Then
+                rootFrame.Navigate(GetType(MainPage), e.Arguments)
+            End If
+
+            Window.Current.Activate()
+        End If
+    End Sub
+
+    Private Sub OnNavigationFailed(sender As Object, e As NavigationFailedEventArgs)
+        Throw New Exception("Failed to load Page " + e.SourcePageType.FullName)
+    End Sub
+
+    Private Sub OnSuspending(sender As Object, e As SuspendingEventArgs) Handles Me.Suspending
+        Dim deferral As SuspendingDeferral = e.SuspendingOperation.GetDeferral()
+        deferral.Complete()
+    End Sub
+
+End Class
